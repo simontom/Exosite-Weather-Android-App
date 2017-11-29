b073f9017927a7fb8054e07034eca72d18aeeceb
simontom <simontom@volny.cz>
2017-11-29 23:29:52 +0100

    Added parseFrom Swapped order of arguments at constructor

--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpctest/model/data_objects/Value.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpctest/model/data_objects/Value.kt
@@ -1,3 +1,9 @@
 package cz.saymon.android.exositeoneplatformrpctest.model.data_objects
 
-data class Value (val value: Double, val time: Long)
+import cz.saymon.android.exositeoneplatformrpctest.model.retrofit.ServerResponse.ServerValue
+
+data class Value(val time: Long, val value: Double) {
+    companion object {
+        fun parseFrom(serverValue: ServerValue) = Value(serverValue.time, serverValue.value)
+    }
+}
