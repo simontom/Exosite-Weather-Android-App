a37d7ac568dde15a7d90097c4f9db97a35ce88f0
simontom <simontom@volny.cz>
2017-11-30 23:45:36 +0100

    Added support for default dataport ids at request

--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpctest/model/retrofit/ServerRequest/ServerRequest.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpctest/model/retrofit/ServerRequest/ServerRequest.kt
@@ -2,7 +2,24 @@ package cz.saymon.android.exositeoneplatformrpctest.model.retrofit.ServerRequest
 
 import com.google.gson.annotations.SerializedName
 
-data class ServerRequest (
-    @SerializedName("auth") val auth: Auth = Auth(),
-    @SerializedName("calls") val calls: List<Call> = Call.defaults
-)
+data class ServerRequest(val dataportIds: List<String>?) {
+
+    constructor() : this(null)
+
+    @SerializedName("auth")
+    val auth: Auth = Auth()
+    @SerializedName("calls")
+    var calls: List<Call> = Call.defaults
+        private set
+
+    init {
+        if (dataportIds != null) {
+            val calls = ArrayList<Call>()
+            for (alias in dataportIds) {
+                calls.add(Call(alias))
+            }
+            this.calls = calls
+        }
+    }
+
+}
