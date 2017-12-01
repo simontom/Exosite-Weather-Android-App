59f330635c94996194fa0e351aaefbaba2401723
simontom <simontom@volny.cz>
2017-12-01 21:07:23 +0100

    Added inject method

--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/dependency_injection/components/AppComponent.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/dependency_injection/components/AppComponent.kt
@@ -5,6 +5,7 @@ import cz.saymon.android.exositeoneplatformrpc.dependency_injection.modules.AppM
 import cz.saymon.android.exositeoneplatformrpc.dependency_injection.modules.NetworkModule
 import cz.saymon.android.exositeoneplatformrpc.model.retrofit.ServerApi
 import cz.saymon.android.exositeoneplatformrpc.ui.activities.MainActivity
+import cz.saymon.android.exositeoneplatformrpc.ui.activities.RecyclerViewActivity
 import dagger.Component
 import retrofit2.Retrofit
 import javax.inject.Singleton
@@ -23,5 +24,6 @@ interface AppComponent {
     // Injections
     //////////////
     fun inject(activity: MainActivity)
+    fun inject(activity: RecyclerViewActivity)
 
 }
