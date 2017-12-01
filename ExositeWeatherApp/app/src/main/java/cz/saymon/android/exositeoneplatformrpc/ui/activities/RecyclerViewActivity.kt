a9edf0bc539eb87a9aef7c66e07ad7dbad08aa2f
simontom <simontom@volny.cz>
2017-12-01 20:47:36 +0100

    New activity with recyclerview

---
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/activities/RecyclerViewActivity.kt
@@ -0,0 +1,12 @@
+package cz.saymon.android.exositeoneplatformrpc.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RecyclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+    }
+}
