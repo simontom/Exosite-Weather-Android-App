9bd122db72a15bf149a7a9cae5f64df9a88d19ec
Simon Tomek <simontom@volny.cz>
2017-12-05 22:33:31 +0100

    Rewritten CallSerializer a bit; It is possible to write to Exosite Portals now;

--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/gsonutils/CallSerializer.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/gsonutils/CallSerializer.kt
@@ -4,6 +4,7 @@ import com.google.gson.*
 import com.google.gson.annotations.SerializedName
 import cz.saymon.android.exositeoneplatformrpc.model.retrofit.request.Argument
 import cz.saymon.android.exositeoneplatformrpc.model.retrofit.request.Call
+import cz.saymon.android.exositeoneplatformrpc.model.retrofit.request.CallProcedureType.*
 import java.lang.reflect.Type
 
 
@@ -14,7 +15,11 @@ class CallSerializer : JsonSerializer<Call> {
 
         addIdProperty(callJsonObject, src)
         addProcedure(callJsonObject, src)
-        addArguments(callJsonObject, context, src)
+
+        when (src.procedure) {
+            READ -> addArgumentsRead(callJsonObject, context, src)
+            WRITE -> addArgumentsWrite(callJsonObject, context, src)
+        }
 
         return callJsonObject
     }
@@ -29,19 +34,36 @@ class CallSerializer : JsonSerializer<Call> {
         callJsonObject.addProperty(procedureSerializedName, src.procedure.procedureName)
     }
 
-    private fun addArguments(callJsonObject: JsonObject, context: JsonSerializationContext, src: Call) {
+    private fun createArgumentWithAlias(context: JsonSerializationContext, src: Call): JsonElement {
         val argumentWithId = Argument.createWithAlias(src.id)
         val argumentWithIdJson = context.serialize(argumentWithId)
+        return argumentWithIdJson
+    }
+
+    private fun addArgumentsJsonArray(callJsonObject: JsonObject, src: Call, vararg argumentsJson: JsonElement) {
+        val argumentsJsonArray = JsonArray()
+        for (value in argumentsJson) {
+            argumentsJsonArray.add(value)
+        }
+
+        val argumentsSerializedName = src.javaClass.getDeclaredField("arguments").getAnnotation(SerializedName::class.java).value
+        callJsonObject.add(argumentsSerializedName, argumentsJsonArray)
+    }
+
+    private fun addArgumentsRead(callJsonObject: JsonObject, context: JsonSerializationContext, src: Call) {
+        val argumentWithIdJson = createArgumentWithAlias(context, src)
 
         val argumentWithOptions = src.arguments.copy(alias = null)
         val argumentWithOptionsJson = context.serialize(argumentWithOptions)
 
-        val argumentsJsonArray = JsonArray()
-        argumentsJsonArray.add(argumentWithIdJson)
-        argumentsJsonArray.add(argumentWithOptionsJson)
+        addArgumentsJsonArray(callJsonObject, src, argumentWithIdJson, argumentWithOptionsJson)
+    }
 
-        val argumentsSerializedName = src.javaClass.getDeclaredField("arguments").getAnnotation(SerializedName::class.java).value
-        callJsonObject.add(argumentsSerializedName, argumentsJsonArray)
+    private fun addArgumentsWrite(callJsonObject: JsonObject, context: JsonSerializationContext, src: Call) {
+        val argumentWithIdJson = createArgumentWithAlias(context, src)
+        val argumentValueJson = context.serialize(src.arguments.writeValue)
+        
+        addArgumentsJsonArray(callJsonObject, src, argumentWithIdJson, argumentValueJson)
     }
 
 }
