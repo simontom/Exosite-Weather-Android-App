4ea084fa36a1d9635116f8ad4b999bded4d25725
Simon Tomek <simontom@volny.cz>
2017-12-06 00:48:50 +0100

    Working on PWM control

--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/request/Argument.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/request/Argument.kt
@@ -9,7 +9,7 @@ data class Argument(
         @SerializedName("alias") val alias: String? = null,
         @Expose(serialize = false) val writeValue: String? = null,
         @SerializedName("starttime") val starttime: Long? = 0,
-        @SerializedName("endtime") val endtime: Long? = (Date().time / 1000.0).roundToLong(), // Because it returns milliseconds
+        @SerializedName("endtime") val endtime: Long? = (Date().time / 1000.0).toLong(), // Because it returns milliseconds
         @SerializedName("sort") val sort: ArgumentSortType? = ArgumentSortType.DESCENDING,
         @SerializedName("limit") val limit: Int? = 1,
         @SerializedName("selection") val selection: ArgumentSelectionType? = ArgumentSelectionType.ALL
--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/activities/PwmControlActivity.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/activities/PwmControlActivity.kt
@@ -2,6 +2,8 @@ package cz.saymon.android.exositeoneplatformrpc.ui.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.SeekBar
+import com.jakewharton.rxbinding2.widget.RxSeekBar
 import cz.saymon.android.exositeoneplatformrpc.R
 import cz.saymon.android.exositeoneplatformrpc.model.Constants
 import cz.saymon.android.exositeoneplatformrpc.model.data_objects.Dataport
@@ -13,6 +15,7 @@ import cz.saymon.android.exositeoneplatformrpc.utils.toast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_pwm_control.*
 import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.dataport_item_row.view.*
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -29,11 +32,25 @@ class PwmControlActivity : AppCompatActivity() {
         appComponent.inject(this)
 
         readServerPwmValues()
+        initPwmSeekbars()
+    }
+
+    private fun initPwmSeekbars() {
+//        RxSeekBar.
     }
 
     private fun handleResponse(dataset: List<Dataport>) {
         toast("onNext: ${System.currentTimeMillis()} ms")
         Timber.d(dataset.toString())
+
+        for (pwmDataport in dataset) {
+            val progress = pwmDataport.values[0].value.toInt()
+            when (pwmDataport.id) {
+                Constants.ALIAS_PWMR -> pwmr.progress = progress
+                Constants.ALIAS_PWMG -> pwmg.progress = progress
+                Constants.ALIAS_PWMB -> pwmb.progress = progress
+            }
+        }
     }
 
     private fun handleResponse(throwable: Throwable) {
