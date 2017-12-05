ce3e36ff78616b925feda17031efc4df194e4132
Simon Tomek <simontom@volny.cz>
2017-12-05 23:42:05 +0100

    Cleaned a bit;

--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/activities/RecyclerViewActivity.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/activities/RecyclerViewActivity.kt
@@ -12,19 +12,12 @@ import cz.saymon.android.exositeoneplatformrpc.model.retrofit.request.ServerRequ
 import cz.saymon.android.exositeoneplatformrpc.ui.recyclerview.DataportRecyclerViewAdapter
 import cz.saymon.android.exositeoneplatformrpc.utils.app
 import cz.saymon.android.exositeoneplatformrpc.utils.toast
-import io.reactivex.Flowable
-import io.reactivex.Observable
-import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Consumer
-import io.reactivex.internal.operators.observable.ObservableDelay
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 import kotlinx.android.synthetic.main.activity_recycler_view.*
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
-import android.util.TypedValue
 
 
 class RecyclerViewActivity : AppCompatActivity() {
@@ -40,55 +33,43 @@ class RecyclerViewActivity : AppCompatActivity() {
 
         app.appComponent.inject(this)
 
-        setRecyclerView()
-        setSwipeRefreshLayout()
+        initRecyclerView()
+        initSwipeRefreshLayout()
 
         callApi()
     }
 
-    private fun setSwipeRefreshLayout() {
+    private fun initSwipeRefreshLayout() {
         swiperefreshlayout.setOnRefreshListener { callApi() }
         swiperefreshlayout.setColorSchemeResources(R.color.blue, R.color.red, R.color.orange,
                 R.color.pink, R.color.yellow, R.color.black, R.color.cyan)
     }
 
-    private fun setRecyclerView() {
+    private fun initRecyclerView() {
         adapter = DataportRecyclerViewAdapter { toast("Clicked on: ${it.id}") }
         recyclerview.layoutManager = LinearLayoutManager(this)
         recyclerview.adapter = adapter
-    }
-
-    private fun stopRefreshingLayout() {
-        if (swiperefreshlayout.isRefreshing) {
-            swiperefreshlayout.isRefreshing = false
-        }
     }
 
     private fun handleResponse(dataset: List<Dataport>) {
         toast("onNext: ${System.currentTimeMillis()}")
         Timber.d(dataset.toString())
         adapter.setDataports(dataset)
-        stopRefreshingLayout()
+        swiperefreshlayout.isRefreshing = false
     }
 
     private fun handleResponse(throwable: Throwable) {
         toast("onException: ${System.currentTimeMillis()}")
         Timber.d(throwable.toString())
-        stopRefreshingLayout()
+        swiperefreshlayout.isRefreshing = false
     }
 
     private fun callApi() {
         swiperefreshlayout.isRefreshing = true
         subscription?.dispose()
 
-//        subscription = Observable.just(true)
-//                .delay(5000, TimeUnit.MILLISECONDS)
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe { swiperefreshlayout.isRefreshing = false }
-
         subscription = api.getItem(ServerRequest(Constants.ALIASES))
-                .delay(1500, TimeUnit.MILLISECONDS)
+                .delay(800, TimeUnit.MILLISECONDS)
                 .flatMapIterable(Dataport.MAPPER)
                 .filter { it.status == DataportStatus.OK }
                 .toSortedList()
