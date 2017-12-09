1ad2204026a879f5ad615001fcb3e07b8e333888
Simon Tomek <simontom@volny.cz>
2017-12-09 22:56:45 +0100

    Implemented Server response deserialiation

--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/gsonutils/ServerResponseDeserializer.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/gsonutils/ServerResponseDeserializer.kt
@@ -1,5 +1,36 @@
 package cz.saymon.android.exositeoneplatformrpc.model.retrofit.gsonutils
 
-/**
- * Created by Simon on 9.12.2017.
- */
+import com.google.gson.*
+import cz.saymon.android.exositeoneplatformrpc.model.retrofit.response.ServerResponse
+import cz.saymon.android.exositeoneplatformrpc.model.retrofit.response.ServerValue
+import java.lang.reflect.Type
+
+class ServerResponseDeserializer : JsonDeserializer<ServerResponse> {
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): ServerResponse {
+        val jsonObject = json as JsonObject
+        val resultJson = jsonObject["result"]
+
+        val id = jsonObject["id"].asString
+        val status = jsonObject["status"].asString
+        val result = when (resultJson) {
+            is JsonArray -> parseResultProperty(resultJson, context)
+            is JsonPrimitive -> parseResultProperty(resultJson, context)
+            else -> throw JsonParseException("Expected JsonArray or JsonPrimitive, got: ${resultJson.javaClass}")
+        }
+
+        return ServerResponse(id, status, result)
+    }
+
+    private fun parseResultProperty(jsonArray: JsonArray, context: JsonDeserializationContext): List<ServerValue> {
+        val listOfServerValues = jsonArray
+                .map { context.deserialize<ServerValue>(it, ServerValue::class.java) }
+                .toList()
+
+        return listOfServerValues
+    }
+
+    private fun parseResultProperty(jsonPrimitive: JsonPrimitive, context: JsonDeserializationContext): List<ServerValue> {
+        val serverValue = context.deserialize<ServerValue>(jsonPrimitive, ServerValue::class.java)
+        return listOf(serverValue)
+    }
+}
--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/gsonutils/ServerValueDeserializer.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/gsonutils/ServerValueDeserializer.kt
@@ -1,5 +1,16 @@
 package cz.saymon.android.exositeoneplatformrpc.model.retrofit.gsonutils
 
-/**
- * Created by Simon on 9.12.2017.
- */
+import com.google.gson.*
+import cz.saymon.android.exositeoneplatformrpc.model.retrofit.response.ServerValue
+import java.lang.reflect.Type
+
+class ServerValueDeserializer : JsonDeserializer<ServerValue> {
+    @Throws(JsonParseException::class)
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): ServerValue {
+        when (json) {
+            is JsonArray -> return ServerValue(json.get(0).asLong, json.get(1).asDouble)
+            is JsonPrimitive -> return ServerValue(json.asLong, 0.0)
+            else -> throw JsonParseException("Expected JsonArray or JsonPrimitive, got: ${json.javaClass}")
+        }
+    }
+}
