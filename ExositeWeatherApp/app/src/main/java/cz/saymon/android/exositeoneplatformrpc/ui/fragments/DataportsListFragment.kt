37b311c9fe1cf6f465d6d32a355dd5dfd23f63cd
Simon Tomek <simontom@volny.cz>
2017-12-16 14:04:04 +0100

    Implemented ListData and PWM functionality; Changed tabbed MainScreen title;

--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/fragments/DataportsListFragment.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/fragments/DataportsListFragment.kt
@@ -13,6 +13,7 @@ import cz.saymon.android.exositeoneplatformrpc.model.data_objects.DataportStatus
 import cz.saymon.android.exositeoneplatformrpc.model.retrofit.ServerApi
 import cz.saymon.android.exositeoneplatformrpc.model.retrofit.request.ServerRequest
 import cz.saymon.android.exositeoneplatformrpc.ui.adapters.DataportRecyclerViewAdapter
+import cz.saymon.android.exositeoneplatformrpc.utils.appComponent
 import cz.saymon.android.exositeoneplatformrpc.utils.toast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -32,54 +33,54 @@ class DataportsListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_dataports_list, container, false)
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-//        appComponent.inject(this)
+        appComponent.inject(this)
 
-//        initRecyclerView()
-//        initSwipeRefreshLayout()
-//        callApi()
-//    }
+        initRecyclerView()
+        initSwipeRefreshLayout()
+        callApi()
+    }
 
-//    private fun initSwipeRefreshLayout() {
-//        swiperefreshlayout.setOnRefreshListener { callApi() }
-//        swiperefreshlayout.setColorSchemeResources(R.color.blue, R.color.red,
-//                R.color.orange, R.color.pink, R.color.yellow, R.color.black, R.color.cyan)
-//    }
-//
-//    private fun initRecyclerView() {
-//        adapter = DataportRecyclerViewAdapter { toast("Clicked on: ${it.id}") }
-//        recyclerview.layoutManager = LinearLayoutManager(context)
-//        recyclerview.adapter = adapter
-//    }
-//
-//    private fun handleResponse(dataset: List<Dataport>) {
-//        toast("onNext: ${System.currentTimeMillis()} ms")
-//        Timber.d(dataset.toString())
-//        adapter.setDataports(dataset)
-//        swiperefreshlayout.isRefreshing = false
-//    }
-//
-//    private fun handleResponse(throwable: Throwable) {
-//        toast("onException: ${System.currentTimeMillis()} ms")
-//        Timber.d(throwable.toString())
-//        swiperefreshlayout.isRefreshing = false
-//    }
-//
-//    private fun callApi() {
-//        swiperefreshlayout.isRefreshing = true
-//        subscription?.dispose()
-//
-//        subscription = api.callRpcApi(ServerRequest(Constants.ALIASES))
-//                .delay(800, TimeUnit.MILLISECONDS)
-//                .flatMapIterable(Dataport.MAPPER)
-//                .filter { it.status == DataportStatus.OK }
-//                .toSortedList()
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(
-//                        { dataset -> handleResponse(dataset) },
-//                        { throwable -> handleResponse(throwable) })
-//    }
+    private fun initSwipeRefreshLayout() {
+        swiperefreshlayout.setOnRefreshListener { callApi() }
+        swiperefreshlayout.setColorSchemeResources(R.color.blue, R.color.red,
+                R.color.orange, R.color.pink, R.color.yellow, R.color.black, R.color.cyan)
+    }
+
+    private fun initRecyclerView() {
+        adapter = DataportRecyclerViewAdapter { toast("Clicked on: ${it.id}") }
+        recyclerview.layoutManager = LinearLayoutManager(context)
+        recyclerview.adapter = adapter
+    }
+
+    private fun handleResponse(dataset: List<Dataport>) {
+        toast("onNext: ${System.currentTimeMillis()} ms")
+        Timber.d(dataset.toString())
+        adapter.setDataports(dataset)
+        swiperefreshlayout.isRefreshing = false
+    }
+
+    private fun handleResponse(throwable: Throwable) {
+        toast("onException: ${System.currentTimeMillis()} ms")
+        Timber.d(throwable.toString())
+        swiperefreshlayout.isRefreshing = false
+    }
+
+    private fun callApi() {
+        swiperefreshlayout.isRefreshing = true
+        subscription?.dispose()
+
+        subscription = api.callRpcApi(ServerRequest(Constants.ALIASES))
+                .delay(800, TimeUnit.MILLISECONDS)
+                .flatMapIterable(Dataport.MAPPER)
+                .filter { it.status == DataportStatus.OK }
+                .toSortedList()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { dataset -> handleResponse(dataset) },
+                        { throwable -> handleResponse(throwable) })
+    }
 
 }
--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/fragments/PwmControlFragment.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/fragments/PwmControlFragment.kt
@@ -7,14 +7,167 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.jakewharton.rxbinding2.widget.RxSeekBar
+import com.jakewharton.rxbinding2.widget.SeekBarProgressChangeEvent
 import kotlinx.android.synthetic.main.fragment_pwm_control.view.*
 
 import cz.saymon.android.exositeoneplatformrpc.R
+import cz.saymon.android.exositeoneplatformrpc.model.Constants
+import cz.saymon.android.exositeoneplatformrpc.model.data_objects.Dataport
+import cz.saymon.android.exositeoneplatformrpc.model.data_objects.DataportStatus
+import cz.saymon.android.exositeoneplatformrpc.model.retrofit.ServerApi
+import cz.saymon.android.exositeoneplatformrpc.model.retrofit.request.Argument
+import cz.saymon.android.exositeoneplatformrpc.model.retrofit.request.ServerRequest
+import cz.saymon.android.exositeoneplatformrpc.model.retrofit.response.ServerResponse
+import cz.saymon.android.exositeoneplatformrpc.utils.appComponent
+import cz.saymon.android.exositeoneplatformrpc.utils.toast
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_pwm_control.*
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 class PwmControlFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_pwm_control, container, false)
+    }
+
+
+
+
+    @Inject
+    lateinit var api: ServerApi
+    private var subscriptionRead: Disposable? = null
+    private var subscriptionWriteR: Disposable? = null
+    private var subscriptionWriteG: Disposable? = null
+    private var subscriptionWriteB: Disposable? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        appComponent.inject(this)
+
+        readServerPwmValues()
+        initPwmSeekbars()
+    }
+
+    private fun initPwmSeekbars() {
+        val flowablePwmR = RxSeekBar.changeEvents(pwmr)
+                .ofType(SeekBarProgressChangeEvent::class.java)
+                .toFlowable(BackpressureStrategy.LATEST)
+                .debounce(Constants.UI_DEBOUNCE_TIME_MS, TimeUnit.MILLISECONDS)
+
+        val flowablePwmG = RxSeekBar.changeEvents(pwmg)
+                .ofType(SeekBarProgressChangeEvent::class.java)
+                .toFlowable(BackpressureStrategy.LATEST)
+                .debounce(Constants.UI_DEBOUNCE_TIME_MS, TimeUnit.MILLISECONDS)
+
+        val flowablePwmB = RxSeekBar.changeEvents(pwmb)
+                .ofType(SeekBarProgressChangeEvent::class.java)
+                .toFlowable(BackpressureStrategy.LATEST)
+                .debounce(Constants.UI_DEBOUNCE_TIME_MS, TimeUnit.MILLISECONDS)
+
+        Flowable.merge(flowablePwmR, flowablePwmG, flowablePwmB)
+                .filter { it.fromUser() }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { seekBarEvent ->
+                    writeServerPwmValueHelper(seekBarEvent.view().id, seekBarEvent.progress())
+                }
+    }
+
+    private fun handleResponse(dataset: List<Dataport>) {
+        toast("onNext (READ): ${System.currentTimeMillis()} ms")
+        Timber.d(dataset.toString())
+
+        for (pwmDataport in dataset) {
+            val progress = pwmDataport.values[0].value.toInt()
+
+            when (pwmDataport.id) {
+                Constants.ALIAS_PWMR -> {
+                    pwmr.progress = progress
+                    pwmr_value.text = progress.toString()
+                }
+                Constants.ALIAS_PWMG -> {
+                    pwmg.progress = progress
+                    pwmg_value.text = progress.toString()
+                }
+                Constants.ALIAS_PWMB -> {
+                    pwmb.progress = progress
+                    pwmb_value.text = progress.toString()
+                }
+            }
+        }
+    }
+
+    private fun handleResponseWrite(dataset: List<ServerResponse>?) {
+        toast("onNext (WRITE): ${System.currentTimeMillis()} ms")
+        Timber.d(dataset.toString())
+    }
+
+    private fun handleResponse(throwable: Throwable) {
+        toast("onException: ${System.currentTimeMillis()} ms")
+        Timber.d(throwable.toString())
+    }
+
+    private fun createWritePwmValueServerRequest(pwmSeekBarAlias: String, progress: Int): ServerRequest {
+        val argument = Argument.createWithAliasWriteValue(pwmSeekBarAlias, progress.toString())
+        val serverRequest = ServerRequest(argument = argument)
+        return serverRequest
+    }
+
+    private fun callWriteApi(serverRequest: ServerRequest): Disposable {
+        return api.callRpcApi(serverRequest)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { responses: List<ServerResponse>? -> handleResponseWrite(responses) },
+                        { throwable -> handleResponse(throwable) }
+                )
+    }
+
+    private fun writeServerPwmRvalue(progress: Int) {
+        pwmr_value.text = progress.toString()
+        val serverRequest = createWritePwmValueServerRequest(Constants.ALIAS_PWMR, progress)
+        subscriptionWriteR?.dispose()
+        subscriptionWriteR = callWriteApi(serverRequest)
+    }
+
+    private fun writeServerPwmGvalue(progress: Int) {
+        pwmg_value.text = progress.toString()
+        val serverRequest = createWritePwmValueServerRequest(Constants.ALIAS_PWMG, progress)
+        subscriptionWriteG?.dispose()
+        subscriptionWriteG = callWriteApi(serverRequest)
+    }
+
+    private fun writeServerPwmBvalue(progress: Int) {
+        pwmb_value.text = progress.toString()
+        val serverRequest = createWritePwmValueServerRequest(Constants.ALIAS_PWMB, progress)
+        subscriptionWriteB?.dispose()
+        subscriptionWriteB = callWriteApi(serverRequest)
+    }
+
+    private fun writeServerPwmValueHelper(seekbarId: Int, seekbarProgress: Int) {
+        when (seekbarId) {
+            pwmr.id -> writeServerPwmRvalue(seekbarProgress)
+            pwmg.id -> writeServerPwmGvalue(seekbarProgress)
+            pwmb.id -> writeServerPwmBvalue(seekbarProgress)
+        }
+    }
+
+    private fun readServerPwmValues() {
+        subscriptionRead?.dispose()
+
+        subscriptionRead = api.callRpcApi(ServerRequest(Constants.ALIASES_PWM))
+                .flatMapIterable(Dataport.MAPPER)
+                .filter { it.status == DataportStatus.OK }
+                .toList()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { dataset -> handleResponse(dataset) },
+                        { throwable -> handleResponse(throwable) })
     }
 
 }
