30373949753c18e73e34e781d07ba9551780faf8
Simon Tomek <simontom@volny.cz>
2017-12-19 23:22:13 +0100

    Added Activity for showing Dataport values in chart

---
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/ui/activities/DataportChartActivity.kt
@@ -0,0 +1,13 @@
+package cz.saymon.android.exositeoneplatformrpc.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cz.saymon.android.exositeoneplatformrpc.R
+
+class DataportChartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dataport_chart)
+    }
+}
