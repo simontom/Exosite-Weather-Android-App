477ee7b43d7d97074b628a42661a409e661e40b1
Simon Tomek <simontom@volny.cz>
2017-12-24 13:34:58 +0100

    Added comments

--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/data_objects/Value.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/data_objects/Value.kt
@@ -4,7 +4,7 @@ import cz.saymon.android.exositeoneplatformrpc.model.retrofit.response.ServerVal
 
 data class Value(val timeMs: Long, val value: Double) {
     companion object {
-        // Multiply by 1000 because Date takes milliseconds
-        fun parseFrom(serverValue: ServerValue) = Value(serverValue.time * 1000, serverValue.value)
+        // Multiply by 1000 because Date expects [ms]
+        fun parseFrom(serverValue: ServerValue) = Value(serverValue.timeS * 1000, serverValue.value)
     }
 }
--- ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/request/ArgumentSortType.kt
+++ ExositeWeatherApp/app/src/main/java/cz/saymon/android/exositeoneplatformrpc/model/retrofit/request/ArgumentSortType.kt
@@ -1,6 +1,18 @@
 package cz.saymon.android.exositeoneplatformrpc.model.retrofit.request
 
+/**
+ * Sort by value time
+ */
 enum class ArgumentSortType(val sortName: String) {
+
+    /**
+     * From older values
+     */
     ASCENDING("asc"),
+
+    /**
+     * From newer values
+     */
     DESCENDING("desc")
+
 }
